40b97833dfdda0d1620b4cc42f2db618c1bb1ab3
AtaeKurri <hakussura@protonmail.com>
2020-09-16 20:25:07 +0200

    Ip edited to be an optional argument. If not ip then it will discover bulbs around the house to find a default ip to connect to

--- appCmd.py
+++ appCmd.py
@@ -2,7 +2,7 @@ import argparse, yeelight, json
 
 def main():
     parse = argparse.ArgumentParser()
-    parse.add_argument("ip", type=str, help="Tells the program what bulb to control.")
+    parse.add_argument("-i", "--ip", type=str, help="Tells the program what bulb to control.")
     parse.add_argument("-p", "--power", help="trigger the power state.", choices=["on", "off"])
     parse.add_argument("-b", "--brightness", type=int, help="Set the brightness to a percentage value.")
     parse.add_argument("-T", "--temp", type=int, help="Set the color temperature.")
@@ -13,43 +13,39 @@ def main():
     args = parse.parse_args()
     global bulb
 
-    try:
-        # Connect with ip to the given bulb.
-        try:
-            bulb = yeelight.Bulb(args.ip)
-        except:
-            try:
-                for bulbs in yeelight.discover_bulbs():
-                    if args.ip in bulbs["capabilities"]["name"]:
-                        bulb = yeelight.Bulb(bulbs["ip"])
-            except:
-                print("Bulb not found.")
+    # Connect with ip to the given bulb.
+    if args.ip: bulb = yeelight.Bulb(args.ip)
+    else:
+        found = False
+        for bulbs in yeelight.discover_bulbs():
+                bulb = yeelight.Bulb(bulbs["ip"])
+                found = True
+        if not found:
+            print("Bulb not found.")
         
-        # Arguments handeling
-        if args.power: # Bulb powering on/off
-            turn_power(args.power)
+    # Arguments handeling
+    if args.power: # Bulb powering on/off
+        turn_power(args.power)
 
-        if args.brightness:
-            bulb.set_brightness(args.brightness)
+    if args.brightness: # Bulb brightness percentage
+        bulb.set_brightness(args.brightness)
 
-        if args.temp:
-            bulb.set_color_temp(args.temp)
+    if args.temp: # Color temperature
+        bulb.set_color_temp(args.temp)
 
-        if args.rgb:
-            rbg = args.rgb
-            bulb.set_rgb(rgb[0], rgb[1], rgb[2])
+    if args.rgb: # Color rgb
+        rbg = args.rgb
+        bulb.set_rgb(rgb[0], rgb[1], rgb[2])
 
-        if args.name:
-            bulb.set_name(args.name)
+    if args.name: # Set bulb nickname (can be seen in the official mobile software too)
+        bulb.set_name(args.name)
 
-        if args.discover:
-            with open("discover.txt", 'w') as f:
-                json.dump(yeelight.discover_bulbs(), f, indent=2)
+    if args.discover: # Create a file with informations on all the bulbs in the network
+        with open("discover.txt", 'w') as f:
+            json.dump(yeelight.discover_bulbs(), f, indent=2)
 
-        if args.toggle:
-            bulb.toggle()
-    except:
-        print("IP field needed.")
+    if args.toggle: # Power toggle
+        bulb.toggle()
 
 def turn_power(state):
     if state == "on":
